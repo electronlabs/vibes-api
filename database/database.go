edd1b560fe05777c9607cea1f8a58395f8d46714
Aris Goudouras <aris.gk@gmail.com>
2018-12-06 19:43:51 +0200

    Removes the database interface

--- database/database.go
+++
@@ -1,7 +0,0 @@
-package database
-
-// Database interface defines basic behavior to interact with a database.
-type Database interface {
-	Connect(uri string) (interface{}, error)
-	Disconnect(client interface{}) error
-}
