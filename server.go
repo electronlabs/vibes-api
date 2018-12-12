b2ec1c30319b641b294581de0a41bdaa59e308f6
Alex Koufatzis <>
2018-12-12 20:21:12 +0100

    sdsd

--- server.go
+++ server.go
@@ -14,7 +14,7 @@ import (
 func main() {
 	configuration := config.NewConfig()
 
-	mongo, err := mongodb.Connect(configuration.MongoURI)
+mongo, err := mongodb.Connect(configuration.MongoURI)
 	if err != nil {
 		panic(err)
 	}
