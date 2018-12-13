e56620429e30672f10df384e88bcb270cd204ea7
DimosthenisBotsaris <botsaris.d@gmail.com>
2018-12-13 10:58:18 +0200

    make auth0 jwt middleware work properly

--- config/config.go
+++ config/config.go
@@ -6,15 +6,27 @@ import (
 
 // Config is a struct that contains configuration variables
 type Config struct {
-	Port     string
-	MongoURI string
+	Environment         string
+	Port                string
+	MongoURI            string
+	JWKSURL             string
+	Auth0NativeClientID string
+	Auth0NativeSecret   string
+	Auth0NativeIssuer   string
+	Auth0APIAudience    string
 }
 
 // NewConfig creates a new Config struct
 func NewConfig() *Config {
 	env.CheckDotEnv()
 	return &Config{
-		Port:     env.MustGet("PORT"),
-		MongoURI: env.MustGet("MONGO_URI"),
+		Environment:         env.MustGet("ENV"),
+		Port:                env.MustGet("PORT"),
+		MongoURI:            env.MustGet("MONGO_URI"),
+		JWKSURL:             env.MustGet("JWKS_URL"),
+		Auth0NativeClientID: env.MustGet("AUTH0_NATIVE_CLIENT_ID"),
+		Auth0NativeSecret:   env.MustGet("AUTH0_NATIVE_SECRET"),
+		Auth0NativeIssuer:   env.MustGet("AUTH0_NATIVE_ISSUER"),
+		Auth0APIAudience:    env.MustGet("AUTH0_API_AUDIENCE"),
 	}
 }
--- router/router.go
+++ router/router.go
@@ -1,22 +1,110 @@
 package router
 
 import (
-	"github.com/electronlabs/vibes-api/domain/actions"
-	actionsRoute "github.com/electronlabs/vibes-api/router/actions"
-	"github.com/electronlabs/vibes-api/router/health"
+	"encoding/json"
+	"errors"
 	"net/http"
 
+	"github.com/electronlabs/vibes-api/domain/actions"
+	actionsRoutes "github.com/electronlabs/vibes-api/router/actions"
+	healthRoutes "github.com/electronlabs/vibes-api/router/actions"
+
+	jwtmiddleware "github.com/auth0/go-jwt-middleware"
+	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/electronlabs/vibes-api/config"
 	"github.com/gin-gonic/gin"
 )
+
+type JSONWebKeys struct {
+	Kty string   `json:"kty"`
+	Kid string   `json:"kid"`
+	Use string   `json:"use"`
+	N   string   `json:"n"`
+	E   string   `json:"e"`
+	X5c []string `json:"x5c"`
+}
+
+type Jwks struct {
+	Keys []JSONWebKeys `json:"keys"`
+}
+
+var configuration = config.NewConfig()
+
+func getPemCert(token *jwt.Token) (string, error) {
+	cert := ""
+	resp, err := http.Get(configuration.JWKSURL)
+
+	if err != nil {
+		return cert, err
+	}
+	defer resp.Body.Close()
+
+	var jwks = Jwks{}
+	err = json.NewDecoder(resp.Body).Decode(&jwks)
+
+	if err != nil {
+		return cert, err
+	}
+
+	for k, _ := range jwks.Keys {
+		if token.Header["kid"] == jwks.Keys[k].Kid {
+			cert = "-----BEGIN CERTIFICATE-----\n" + jwks.Keys[k].X5c[0] + "\n-----END CERTIFICATE-----"
+		}
+	}
+
+	if cert == "" {
+		err := errors.New("Unable to find appropriate key.")
+		return cert, err
+	}
+
+	return cert, nil
+}
+
+var jwtMiddleware = jwtmiddleware.New(jwtmiddleware.Options{
+	ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+		// Verify 'aud' claim
+		aud := configuration.Auth0APIAudience
+		checkAud := token.Claims.(jwt.MapClaims).VerifyAudience(aud, false)
+		if !checkAud {
+			return token, errors.New("Invalid audience.")
+		}
+		// Verify 'iss' claim
+		iss := configuration.Auth0NativeIssuer
+		checkIss := token.Claims.(jwt.MapClaims).VerifyIssuer(iss, false)
+		if !checkIss {
+			return token, errors.New("Invalid issuer.")
+		}
+
+		cert, err := getPemCert(token)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		result, _ := jwt.ParseRSAPublicKeyFromPEM([]byte(cert))
+		return result, nil
+	},
+	SigningMethod: jwt.SigningMethodRS256,
+})
+
+func checkJWT() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		jwtMid := *jwtMiddleware
+		if err := jwtMid.CheckJWT(c.Writer, c.Request); err != nil {
+			c.AbortWithStatus(401)
+		}
+	}
+}
 
 // NewHTTPHandler returns the HTTP requests handler
 func NewHTTPHandler(actionsSvc *actions.Service) http.Handler {
 	router := gin.Default()
-	health.Routes(router)
+
+	healthGroup := router.Group("/health")
+	healthRoutes.NewRoutesFactory(actionsSvc)(healthGroup)
 
 	api := router.Group("/api")
-
 	actionsGroup := api.Group("/actions")
-	actionsRoute.NewRoutesFactory(actionsSvc)(actionsGroup)
+	actionsGroup.Use(checkJWT())
+	actionsRoutes.NewRoutesFactory(actionsSvc)(actionsGroup)
 	return router
 }
